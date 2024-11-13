acf781fb3c22ec09636a4d848949403db4f545bc
Sarvesh843 <sarveshsamsher.yadav@attplgroup.com>
2024-11-13 20:38:44 +0530

    Update page.tsx

--- admin-dashboard/src/app/admin/users/[id]/page.tsx
+++ admin-dashboard/src/app/admin/users/[id]/page.tsx
@@ -1,3 +1,5 @@
+// import { cookies } from 'next/headers';
+// import { redirect } from 'next/navigation';
 import React from 'react';
 
 interface User {
@@ -6,10 +8,21 @@ interface User {
   email: string;
   contactNumber: string;
   profilePicture: string;
-  address: string;
+  address:string;
 }
 
 async function fetchUserDetails(id: string): Promise<User> {
+  // try {
+  //   const response = await axios.get('https://eazrdaily.eazr.in//admin/users/[id]', {
+  //     headers: {
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //   });
+  //   return response.data;
+  // } catch (error) {
+  //   console.error('Error fetching user:', error);
+  //   return [];
+  // }
   const dummyUser = {
     id: id,
     name: 'John Doe',
@@ -17,15 +30,27 @@ async function fetchUserDetails(id: string): Promise<User> {
     contactNumber: '123-456-7890',
     profilePicture: 'https://via.placeholder.com/150',
     address: '123 Main St, Springfield, IL, 62701',
+
   };
   return dummyUser;
 }
 
-const UserProfile = async ({ params }: { params: { id: string } }) => {
-  const user = await fetchUserDetails(params.id);
+const UserProfile = async ({ params }: { params: Promise<{ id: string }> }) => {
+//   const token = cookies().get('token')?.value;
+// const token ="dummytoken"
+
+//   if (!token) {
+//     redirect('/admin/login');
+//   }
+
+//   const user = await fetchUserDetails(params.id, token);// if token is provided
+  const{id} = await params;
+
+  const user = await fetchUserDetails(id);
+
 
   return (
-    <div style={{ padding: '32px', fontFamily: 'Arial, sans-serif', backgroundColor: '#f9fafb', height: "100vh" }}>
+    <div style={{ padding: '32px', fontFamily: 'Arial, sans-serif', backgroundColor: '#f9fafb', height:"100vh" }}>
       <h1
         style={{
           fontSize: '2.25rem',
@@ -53,7 +78,7 @@ const UserProfile = async ({ params }: { params: { id: string } }) => {
             height: '300px',
             borderRadius: '50%',
             border: '2px solid #d1d5db',
-            marginRight: '24px',
+            marginRight: '24px', 
           }}
         />
 
