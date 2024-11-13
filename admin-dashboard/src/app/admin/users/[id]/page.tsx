0b545c515f45c876c4173b8caae4a304617d78da
Sarvesh843 <sarveshsamsher.yadav@attplgroup.com>
2024-11-13 20:07:51 +0530

    Update page.tsx

--- admin-dashboard/src/app/admin/users/[id]/page.tsx
+++ admin-dashboard/src/app/admin/users/[id]/page.tsx
@@ -1,5 +1,3 @@
-// import { cookies } from 'next/headers';
-// import { redirect } from 'next/navigation';
 import React from 'react';
 
 interface User {
@@ -8,21 +6,10 @@ interface User {
   email: string;
   contactNumber: string;
   profilePicture: string;
-  address:string;
+  address: string;
 }
 
 async function fetchUserDetails(id: string): Promise<User> {
-  // try {
-  //   const response = await axios.get('https://eazrdaily.eazr.in//admin/users/[id]', {
-  //     headers: {
-  //       Authorization: `Bearer ${token}`,
-  //     },
-  //   });
-  //   return response.data;
-  // } catch (error) {
-  //   console.error('Error fetching user:', error);
-  //   return [];
-  // }
   const dummyUser = {
     id: id,
     name: 'John Doe',
@@ -30,26 +17,15 @@ async function fetchUserDetails(id: string): Promise<User> {
     contactNumber: '123-456-7890',
     profilePicture: 'https://via.placeholder.com/150',
     address: '123 Main St, Springfield, IL, 62701',
-
   };
   return dummyUser;
 }
 
 const UserProfile = async ({ params }: { params: { id: string } }) => {
-//   const token = cookies().get('token')?.value;
-// const token ="dummytoken"
-
-//   if (!token) {
-//     redirect('/admin/login');
-//   }
-
-//   const user = await fetchUserDetails(params.id, token);// if token is provided
-
-  const user = await fetchUserDetails(params.id,);
-
+  const user = await fetchUserDetails(params.id);
 
   return (
-    <div style={{ padding: '32px', fontFamily: 'Arial, sans-serif', backgroundColor: '#f9fafb', height:"100vh" }}>
+    <div style={{ padding: '32px', fontFamily: 'Arial, sans-serif', backgroundColor: '#f9fafb', height: "100vh" }}>
       <h1
         style={{
           fontSize: '2.25rem',
@@ -77,7 +53,7 @@ const UserProfile = async ({ params }: { params: { id: string } }) => {
             height: '300px',
             borderRadius: '50%',
             border: '2px solid #d1d5db',
-            marginRight: '24px', // Space between picture and details
+            marginRight: '24px',
           }}
         />
 
