8387a3c0b40b63b4cf6f41244459c5414981b999
Sarvesh843 <sarveshsamsher.yadav@attplgroup.com>
2024-11-13 19:11:08 +0530

    redirect debug

--- admin-dashboard/src/app/admin/dashboard/page.tsx
+++ admin-dashboard/src/app/admin/dashboard/page.tsx
@@ -1,5 +1,5 @@
 // import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
+// import { redirect } from 'next/navigation';
 import Link from 'next/link';
 import React from 'react';
 
--- admin-dashboard/src/app/admin/users/[userId]/page.tsx
+++ admin-dashboard/src/app/admin/users/[userId]/page.tsx
@@ -1,5 +1,5 @@
 // import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
+// import { redirect } from 'next/navigation';
 import React from 'react';
 
 interface User {
